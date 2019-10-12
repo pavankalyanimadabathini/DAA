1ea2126a115c19238cd6e7d5897178b62d73651a
pavankalyanimadabathini <48764540+pavankalyanimadabathini@users.noreply.github.com>
2019-10-12 13:16:49 +0530

    Update and rename README.md to labcycle1.c

---
+++ labcycle1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+void minMax(int *a,int l,int h,int *min,int *max)
+{
+    int m,min1,max1;
+    if(l == h){
+        *min = a[l];
+        *max = a[h];
+    }
+    else if(l+1 == h){
+        if(a[l]<a[h]){
+            *min = a[l];
+            *max = a[h];
+        }
+        else{
+            *min = a[h];
+            *max = a[l];
+        }
+    }
+    else{
+        m=(l+h)/2;
+        minMax(a,l,m,min,max);
+        minMax(a,m+1,h,&min1,&max1);
+        if(min1 < *min)
+            *min = min1;
+        if(max1 > *max)
+            *max = max1;
+    }
+}
+int main()
+{
+    int *a,n,i,min,max;
+    printf("enter size of array:");
+    scanf("%d",&n);
+    for(i = 0; i < n; i++)
+        scanf("%d", a+i);
+    minMax(a,0,n-1,&min,&max);
+    printf("Minimum = %d \n", min);
+    printf("Maximum = %d \n", max);
+    return 0;
+}
